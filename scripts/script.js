85c258e0d271a8fe2d3601ddf6a0ea2fd60f765a
origenfood <51681165+origenfood@users.noreply.github.com>
2019-06-13 15:13:22 -0500

    Update 360 - Restaurant
    Added the links for the 360 photos and the photos from the menu of the restaurant

--- scripts/script.js
+++ scripts/script.js
@@ -40,7 +40,7 @@ function showDivsDbl(n) {
 
 /*-------------------Slideshow 3-------------------*/
 var slideIndex3 = 1;
-showDivsDbl(slideIndex3);
+showDivs3(slideIndex3);
 
 function plusDivs3(n) {
   showDivs3(slideIndex3 += n);
@@ -60,7 +60,7 @@ function showDivs3(n) {
 
 /*-------------------Slideshow 4-------------------*/
 var slideIndex4 = 1;
-showDivsDbl(slideIndex4);
+showDivs4(slideIndex4);
 
 function plusDivs4(n) {
   showDivs4(slideIndex4 += n);
@@ -77,6 +77,106 @@ function showDivs4(n) {
   x[slideIndex4-1].style.display = "block";
 }
 /*-------------------Slideshow 4-------------------*/
+
+/*-------------------Slideshow 5-------------------*/
+var slideIndex5 = 1;
+showDivs5(slideIndex5);
+
+function plusDivs5(n) {
+  showDivs5(slideIndex5 += n);
+}
+
+function showDivs5(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides5");
+  if (n > x.length) {slideIndex5 = 1}
+  if (n < 1) {slideIndex5 = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex5-1].style.display = "block";
+}
+/*-------------------Slideshow 5-------------------*/
+
+/*-------------------Slideshow 6-------------------*/
+var slideIndex6 = 1;
+showDivs6(slideIndex6);
+
+function plusDivs6(n) {
+  showDivs6(slideIndex6 += n);
+}
+
+function showDivs6(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides6");
+  if (n > x.length) {slideIndex6 = 1}
+  if (n < 1) {slideIndex6 = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex6-1].style.display = "block";
+}
+/*-------------------Slideshow 6-------------------*/
+
+/*-------------------Slideshow 7-------------------*/
+var slideIndex7 = 1;
+showDivs7(slideIndex7);
+
+function plusDivs7(n) {
+  showDivs7(slideIndex7 += n);
+}
+
+function showDivs7(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides7");
+  if (n > x.length) {slideIndex7 = 1}
+  if (n < 1) {slideIndex7 = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex7-1].style.display = "block";
+}
+/*-------------------Slideshow 7-------------------*/
+
+/*-------------------Slideshow 8-------------------*/
+var slideIndex8 = 1;
+showDivs8(slideIndex8);
+
+function plusDivs8(n) {
+  showDivs8(slideIndex8 += n);
+}
+
+function showDivs8(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides8");
+  if (n > x.length) {slideIndex8 = 1}
+  if (n < 1) {slideIndex8 = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex8-1].style.display = "block";
+}
+/*-------------------Slideshow 8-------------------*/
+
+/*-------------------Slideshow 9-------------------*/
+var slideIndex9 = 1;
+showDivs9(slideIndex9);
+
+function plusDivs9(n) {
+  showDivs9(slideIndex9 += n);
+}
+
+function showDivs9(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides9");
+  if (n > x.length) {slideIndex9 = 1}
+  if (n < 1) {slideIndex9 = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex9-1].style.display = "block";
+}
+/*-------------------Slideshow 9-------------------*/
 
 function landing(){
   var lang = navigator.language.split("-");
